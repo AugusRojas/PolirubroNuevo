f869a14e80d1ad8fc8940058e05de6a2ccbf0c2c
AugusRojas <augus32132@gmail.com>
2024-11-13 08:16:47 -0300

    Perro

--- POLIRUBRO/Program.cs
+++ POLIRUBRO/Program.cs
@@ -17,7 +17,7 @@ namespace POLIRUBRO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new categoria_metodo_pago());
+            Application.Run(new capaPresentacion.Facturacion());
         }
     }
 }
