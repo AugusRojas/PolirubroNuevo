755dc1218628924d4b09b7ab72f2372267347ff9
Lisandro Gabriel <168690237+LisandroGabrielReinoso@users.noreply.github.com>
2024-11-17 13:31:40 -0300

    Cambios 17-11

--- POLIRUBRO/capaDatos/Conexion.cs
+++ POLIRUBRO/capaDatos/Conexion.cs
@@ -5,12 +5,8 @@ namespace POLIRUBRO
 {
     public static class Conexion
     {
-<<<<<<< HEAD
-        static string conexion = "server=DESKTOP-4TIIP6L\\SQLEXPRESS ; database=sistema_de_ventas ; integrated security=true";
-=======
         // Cambia esta cadena de conexión para que apunte a tu base de datos MySQL en XAMPP
         static string conexion = "Server=AUGUSTO\\SQLEXPRESS; Database=sistema_de_ventas; Integrated Security=True;";
->>>>>>> 12c0fafb6b43d9d74eced38ccc5888ada47fe6c9
 
         // Método para obtener la conexión a la base de datos MySQL
         public static SqlConnection obtenerConexion()
@@ -24,11 +20,7 @@ namespace POLIRUBRO
             }
             catch (Exception ex)
             {
-<<<<<<< HEAD
-                MessageBox.Show(ex.Message);
-=======
                 Console.WriteLine("Error de conexión: " + ex.Message);
->>>>>>> 12c0fafb6b43d9d74eced38ccc5888ada47fe6c9
                 return null;
             }
         }
