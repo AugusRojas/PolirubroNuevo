4b18703c7d4a2a019f0e2b7d38c7e548290248c1
AugusRojas <augus32132@gmail.com>
2024-11-28 16:10:14 -0300

    1.0.2 cambios

--- capaDatos/Conexion.cs
+++ capaDatos/Conexion.cs
@@ -6,7 +6,8 @@ namespace POLIRUBRO
     public static class Conexion
     {
         // Cambia esta cadena de conexión para que apunte a tu base de datos MySQL en XAMPP
-        static string conexion = "Server=DESKTOP-4TIIP6L\\SQLEXPRESS; Database=sistema_de_ventas; Integrated Security=True;";
+        static string conexion = "Server=tcp:sql9001.site4now.net,1433;Initial Catalog=db_aafc92_sistemaventas;Persist Security Info=False;User ID=db_aafc92_sistemaventas_admin;Password=ventas2000\r\n;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
 
         // Método para obtener la conexión a la base de datos MySQL
         public static SqlConnection obtenerConexion()
