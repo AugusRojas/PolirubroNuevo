2c3a212328dd17286fd8579aa32e5ec8a7f15b71
Lisandro Gabriel <saltodecuenta2023@gmail.com>
2025-03-20 00:45:28 -0300

    solo diseño 20/03

--- capaPresentacion/categoria_metodo_pago.Designer.cs
+++ capaPresentacion/categoria_metodo_pago.Designer.cs
@@ -80,6 +80,8 @@
             // 
             // boxPago
             // 
+            this.boxPago.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.boxPago.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.boxPago.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.boxPago.FormattingEnabled = true;
             this.boxPago.Location = new System.Drawing.Point(193, 37);
@@ -87,19 +89,21 @@
             this.boxPago.Name = "boxPago";
             this.boxPago.Size = new System.Drawing.Size(210, 21);
             this.boxPago.TabIndex = 7;
-            this.boxPago.Text = "Seleccione un metodo de pago...";
             // 
             // btnEliminarPago
             // 
             this.btnEliminarPago.BackColor = System.Drawing.Color.LightCoral;
             this.btnEliminarPago.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnEliminarPago.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminarPago.Location = new System.Drawing.Point(39, 98);
+            this.btnEliminarPago.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarPago.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminarPago.Image")));
+            this.btnEliminarPago.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminarPago.Location = new System.Drawing.Point(8, 97);
             this.btnEliminarPago.Margin = new System.Windows.Forms.Padding(2);
             this.btnEliminarPago.Name = "btnEliminarPago";
-            this.btnEliminarPago.Size = new System.Drawing.Size(80, 29);
+            this.btnEliminarPago.Size = new System.Drawing.Size(159, 29);
             this.btnEliminarPago.TabIndex = 6;
-            this.btnEliminarPago.Text = "Eliminar";
+            this.btnEliminarPago.Text = "Eliminar Metodo";
+            this.btnEliminarPago.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEliminarPago.UseVisualStyleBackColor = false;
             this.btnEliminarPago.Click += new System.EventHandler(this.btnEliminarPago_Click);
             // 
@@ -116,7 +120,7 @@
             this.groupCategoria.Margin = new System.Windows.Forms.Padding(2);
             this.groupCategoria.Name = "groupCategoria";
             this.groupCategoria.Padding = new System.Windows.Forms.Padding(2);
-            this.groupCategoria.Size = new System.Drawing.Size(343, 154);
+            this.groupCategoria.Size = new System.Drawing.Size(362, 154);
             this.groupCategoria.TabIndex = 1;
             this.groupCategoria.TabStop = false;
             this.groupCategoria.Text = "   Categoria   ";
@@ -126,7 +130,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(4, 29);
+            this.label3.Location = new System.Drawing.Point(14, 29);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(136, 20);
@@ -135,36 +139,42 @@
             // 
             // boxCategoria
             // 
+            this.boxCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.boxCategoria.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.boxCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.boxCategoria.FormattingEnabled = true;
-            this.boxCategoria.Location = new System.Drawing.Point(146, 29);
+            this.boxCategoria.Location = new System.Drawing.Point(166, 31);
             this.boxCategoria.Margin = new System.Windows.Forms.Padding(2);
             this.boxCategoria.Name = "boxCategoria";
             this.boxCategoria.Size = new System.Drawing.Size(172, 21);
             this.boxCategoria.TabIndex = 2;
-            this.boxCategoria.Text = "Seleccione una categoria...";
             // 
             // btnEliminarCategoria
             // 
             this.btnEliminarCategoria.BackColor = System.Drawing.Color.LightCoral;
             this.btnEliminarCategoria.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnEliminarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminarCategoria.Location = new System.Drawing.Point(39, 78);
+            this.btnEliminarCategoria.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarCategoria.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminarCategoria.Image")));
+            this.btnEliminarCategoria.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminarCategoria.Location = new System.Drawing.Point(8, 112);
             this.btnEliminarCategoria.Margin = new System.Windows.Forms.Padding(2);
             this.btnEliminarCategoria.Name = "btnEliminarCategoria";
-            this.btnEliminarCategoria.Size = new System.Drawing.Size(79, 29);
+            this.btnEliminarCategoria.Size = new System.Drawing.Size(171, 29);
             this.btnEliminarCategoria.TabIndex = 4;
-            this.btnEliminarCategoria.Text = "Eliminar";
+            this.btnEliminarCategoria.Text = "Eliminar categoria";
+            this.btnEliminarCategoria.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEliminarCategoria.UseVisualStyleBackColor = false;
             this.btnEliminarCategoria.Click += new System.EventHandler(this.btnEliminarCategoria_Click);
             // 
             // Unidad
             // 
+            this.Unidad.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Unidad.BackgroundImage")));
             this.Unidad.Controls.Add(this.btnEliminar);
             this.Unidad.Controls.Add(this.label1);
             this.Unidad.Controls.Add(this.boxUnidad);
             this.Unidad.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Unidad.Location = new System.Drawing.Point(465, 202);
+            this.Unidad.ForeColor = System.Drawing.SystemColors.Control;
+            this.Unidad.Location = new System.Drawing.Point(483, 183);
             this.Unidad.Margin = new System.Windows.Forms.Padding(2);
             this.Unidad.Name = "Unidad";
             this.Unidad.Padding = new System.Windows.Forms.Padding(2);
@@ -177,37 +187,43 @@
             // 
             this.btnEliminar.BackColor = System.Drawing.Color.LightCoral;
             this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(192, 64);
+            this.btnEliminar.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(23, 64);
             this.btnEliminar.Margin = new System.Windows.Forms.Padding(2);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(79, 29);
+            this.btnEliminar.Size = new System.Drawing.Size(157, 29);
             this.btnEliminar.TabIndex = 9;
-            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.Text = "Eliminar Unidad";
+            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEliminar.UseVisualStyleBackColor = false;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
             this.label1.Location = new System.Drawing.Point(19, 27);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 15);
+            this.label1.Size = new System.Drawing.Size(120, 20);
             this.label1.TabIndex = 9;
             this.label1.Text = "Eliminar Unidad";
             // 
             // boxUnidad
             // 
+            this.boxUnidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.boxUnidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.boxUnidad.FormattingEnabled = true;
-            this.boxUnidad.Location = new System.Drawing.Point(148, 21);
+            this.boxUnidad.Location = new System.Drawing.Point(148, 29);
             this.boxUnidad.Margin = new System.Windows.Forms.Padding(2);
             this.boxUnidad.Name = "boxUnidad";
-            this.boxUnidad.Size = new System.Drawing.Size(144, 21);
+            this.boxUnidad.Size = new System.Drawing.Size(152, 21);
             this.boxUnidad.TabIndex = 3;
-            this.boxUnidad.Text = "Seleccione una unidad...";
             // 
             // categoria_metodo_pago
             // 
