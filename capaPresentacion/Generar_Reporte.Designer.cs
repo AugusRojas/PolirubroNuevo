3bbf4edc5907f67f98fa56c72f36486c1bf05c2f
Ramiro <rrahmanrintoul@gmail.com>
2025-05-01 15:57:25 -0300

    Cambios en generar reporte, agregado monthPicker, funciones para generar reportes según mes y año que decida el usuario, y genera tanto pdf como excel

--- capaPresentacion/Generar_Reporte.Designer.cs
+++ capaPresentacion/Generar_Reporte.Designer.cs
@@ -30,21 +30,30 @@
         {
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.Mes = new System.Windows.Forms.CheckBox();
+            this.Anio = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBoxAnio = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Black;
             this.button2.Image = global::POLIRUBRO.Properties.Resources.icons8_excel_150;
-            this.button2.Location = new System.Drawing.Point(312, 110);
+            this.button2.Location = new System.Drawing.Point(208, 11);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(198, 205);
+            this.button2.Size = new System.Drawing.Size(198, 278);
             this.button2.TabIndex = 0;
-            this.button2.Text = "GENERAR EXCEL";
+            this.button2.Text = "GENERAR EXCEL GENERAL";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -52,31 +61,154 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Image = global::POLIRUBRO.Properties.Resources.icons8_pdf_195;
-            this.button1.Location = new System.Drawing.Point(99, 110);
+            this.button1.Location = new System.Drawing.Point(11, 11);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 205);
+            this.button1.Size = new System.Drawing.Size(193, 278);
             this.button1.TabIndex = 0;
-            this.button1.Text = "GENERAR PDF";
+            this.button1.Text = "GENERAR PDF GENERAL";
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Mes
+            // 
+            this.Mes.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Mes.AutoSize = true;
+            this.Mes.BackColor = System.Drawing.Color.LightCoral;
+            this.Mes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Mes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Mes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mes.Location = new System.Drawing.Point(463, 46);
+            this.Mes.Name = "Mes";
+            this.Mes.Size = new System.Drawing.Size(78, 41);
+            this.Mes.TabIndex = 1;
+            this.Mes.Text = "Mes";
+            this.Mes.UseVisualStyleBackColor = false;
+            this.Mes.CheckedChanged += new System.EventHandler(this.Mes_CheckedChanged);
+            // 
+            // Anio
+            // 
+            this.Anio.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Anio.AutoSize = true;
+            this.Anio.BackColor = System.Drawing.Color.LightCoral;
+            this.Anio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Anio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Anio.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Anio.Location = new System.Drawing.Point(463, 248);
+            this.Anio.Name = "Anio";
+            this.Anio.Size = new System.Drawing.Size(75, 41);
+            this.Anio.TabIndex = 1;
+            this.Anio.Text = "Año";
+            this.Anio.UseVisualStyleBackColor = false;
+            this.Anio.CheckedChanged += new System.EventHandler(this.Anio_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(411, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccionar Mes o año";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightCoral;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(586, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 64);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "GENERAR PDF MENSUAL";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightCoral;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(689, 225);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 64);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "GENERAR EXCEL MENSUAL";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBoxAnio
+            // 
+            this.comboBoxAnio.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBoxAnio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBoxAnio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAnio.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBoxAnio.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxAnio.FormattingEnabled = true;
+            this.comboBoxAnio.Location = new System.Drawing.Point(586, 46);
+            this.comboBoxAnio.Name = "comboBoxAnio";
+            this.comboBoxAnio.Size = new System.Drawing.Size(200, 25);
+            this.comboBoxAnio.TabIndex = 4;
+            this.comboBoxAnio.Visible = false;
+            this.comboBoxAnio.SelectedIndexChanged += new System.EventHandler(this.comboBoxAnio_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightCoral;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(586, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 64);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "GENERAR PDF ANUAL";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightCoral;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(689, 225);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 64);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "GENERAR EXCEL ANUAL";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Generar_Reporte
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightPink;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(850, 365);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBoxAnio);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Anio);
+            this.Controls.Add(this.Mes);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Generar_Reporte";
             this.Text = "Generar_Reporte";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -84,5 +216,13 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox Mes;
+        private System.Windows.Forms.CheckBox Anio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBoxAnio;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- capaPresentacion/Generar_Reporte.cs
+++ capaPresentacion/Generar_Reporte.cs
@@ -1,16 +1,60 @@
 ﻿using System;
 using System.Data;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using DocumentFormat.OpenXml.Bibliography;
 using POLIRUBRO.capaNegocio;
 
 namespace POLIRUBRO.capaPresentacion
 {
     public partial class Generar_Reporte : Form
     {
+        private monthPicker monthPicker;
+
         public Generar_Reporte()
         {
             InitializeComponent();
+
+            // Cargar años desde 2024 hasta el año actual en el comboBoxAnio
+            comboBoxAnio.Items.Add("Seleccionar Año");
+            int primerAnio = 2024;
+            int anioActual = DateTime.Now.Year;
+
+            for (int anio = primerAnio; anio <= anioActual; anio++)
+            {
+                comboBoxAnio.Items.Add(anio);
+            }
+            comboBoxAnio.SelectedIndex = 0;
+
+            // Agregar MonthPicker
+            monthPicker = new monthPicker();
+            monthPicker.Location = new Point(575, 47); // Posición en el formulario
+            monthPicker.Width = 240;
+            monthPicker.Height = 140;
+            monthPicker.Format = DateTimePickerFormat.Custom;
+            monthPicker.CustomFormat = "MMMM yyyy";
+            monthPicker.ShowUpDown = false;
+            monthPicker.MinDate = new DateTime(2024, 12, 1); // Fecha de inicio (Segun DB)
+            monthPicker.MaxDate = DateTime.Today;   // Hasta hoy
+            monthPicker.Font = new Font("Segoe UI", 10, FontStyle.Regular);
+            monthPicker.BackColor = Color.White;
+            monthPicker.ForeColor = Color.DarkSlateGray;
+            Controls.Add(monthPicker);
+
+
+            monthPicker.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+
+            // Agregar Label descriptivo
+            Label lblMes = new Label();
+            lblMes.Text = "Seleccioná un mes:";
+            lblMes.Location = new Point(40, 20);
+            lblMes.AutoSize = true;
+            this.Controls.Add(lblMes);
         }
 
         // generar PDF
@@ -23,13 +67,21 @@ namespace POLIRUBRO.capaPresentacion
                 string rutaDestino = creacionCarpetas.Crear_carpeta_reporte();
 
                 Reporte reporte = new Reporte();
-                DataTable data = reporte.obtenerDatosReporte();
+                DataTable data = reporte.obtenerDatosReporte(); 
 
                 string rutaPDF = Path.Combine(rutaDestino, "Reporte.pdf");
 
                 reporte.generarReportePDF(data, rutaPDF);
 
                 MessageBox.Show($"Reporte en PDF generado exitosamente en:\n{rutaPDF}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                var proceso = new System.Diagnostics.Process();
+                proceso.StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = rutaPDF,
+                    UseShellExecute = true
+                };
+                proceso.Start();
             }
             catch (Exception ex)
             {
@@ -47,18 +99,203 @@ namespace POLIRUBRO.capaPresentacion
                 string rutaDestino = creacion.Crear_carpeta_reporte();
 
                 Reporte reporte = new Reporte();
-                DataTable data = reporte.obtenerDatosReporte();
+                DataTable data = reporte.obtenerDatosReporte(); 
 
                 string rutaExcel = Path.Combine(rutaDestino, "Reporte.xlsx");
 
                 reporte.generarReporteExcel(data, rutaExcel);
 
                 MessageBox.Show($"Reporte en Excel generado exitosamente en:\n{rutaExcel}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                var proceso = new System.Diagnostics.Process();
+                proceso.StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = rutaExcel,
+                    UseShellExecute = true
+                };
+                proceso.Start();
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Error al generar el reporte: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        private void Mes_CheckedChanged(object sender, EventArgs e)
+        {
+            monthPicker.Visible = Mes.Checked;
+            button3.Visible = Mes.Checked;
+            button4.Visible = Mes.Checked;
+
+            if (Mes.Checked)
+                Anio.Checked = false;
+        }
+
+        private void Anio_CheckedChanged(object sender, EventArgs e)
+        {
+            monthPicker.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+
+            if (Anio.Checked)
+            {
+                Mes.Checked = false;
+                comboBoxAnio.Visible = true;
+                button5.Visible = true;
+                button6.Visible = true;
+            }
+            else
+            {
+                comboBoxAnio.Visible = false;
+                button5.Visible = false;
+                button6.Visible = false;
+            }
+        }
+
+        // Botón para PDF Mensual
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Creacion_dinamica_carpetas creacion = new Creacion_dinamica_carpetas();
+                string rutaDestino = creacion.Crear_carpeta_reporte();
+                Reporte reporte = new Reporte();
+
+                int mes = monthPicker.Value.Month;
+                int anio = monthPicker.Value.Year;
+
+                DataTable data = reporte.ObtenerDatosReportesPorMes(mes, anio);
+                string rutaPDF = Path.Combine(rutaDestino, "Reporte_Mensual.pdf");
+
+                reporte.generarReportePDF(data, rutaPDF);
+                MessageBox.Show($"Reporte en PDF Mensual generado exitosamente en:\n{rutaPDF}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Abrir el PDF automáticamente
+                var proceso = new System.Diagnostics.Process();
+                proceso.StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = rutaPDF,
+                    UseShellExecute = true
+                };
+                proceso.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al generar el reporte PDF mensual: {ex.Message}");
+            }
+        }
+
+        // Botón para Excel Mensual
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Creacion_dinamica_carpetas creacion = new Creacion_dinamica_carpetas();
+                string rutaDestino = creacion.Crear_carpeta_reporte();
+                Reporte reporte = new Reporte();
+
+                int mes = monthPicker.Value.Month;
+                int anio = monthPicker.Value.Year;
+
+                string rutaExcel = Path.Combine(rutaDestino, "Reporte_Mensual.xlsx");
+
+                reporte.generarReporteExcelPorMes(mes, anio, rutaExcel);
+                MessageBox.Show($"Reporte en Excel Mensual generado exitosamente en:\n{rutaExcel}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Abrir el Excel automáticamente
+                var proceso = new System.Diagnostics.Process();
+                proceso.StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = rutaExcel,
+                    UseShellExecute = true
+                };
+                proceso.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al generar el reporte Excel mensual: {ex.Message}");
+            }
+        }
+
+        private void comboBoxAnio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Le hice doble click al comboBox, y ya no puedo eliminar esto xd    
+        }
+
+        // Botón Generar Reporte Anual PDF
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBoxAnio.SelectedItem == null)
+                {
+                    MessageBox.Show("Por favor seleccioná un año.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int anio = (int)comboBoxAnio.SelectedItem;
+
+                Creacion_dinamica_carpetas creacion = new Creacion_dinamica_carpetas();
+                string rutaDestino = creacion.Crear_carpeta_reporte();
+                Reporte reporte = new Reporte();
+
+                DataTable data = reporte.ObtenerDatosReportesPorAnio(anio);
+                string rutaPDF = Path.Combine(rutaDestino, $"Reporte_Anual_{anio}.pdf");
+
+                reporte.generarReportePDF(data, rutaPDF);
+
+                MessageBox.Show($"Reporte Anual en PDF generado exitosamente en:\n{rutaPDF}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                var proceso = new System.Diagnostics.Process();
+                proceso.StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = rutaPDF,
+                    UseShellExecute = true
+                };
+                proceso.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al generar el reporte PDF anual: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Botón Generar Reporte Anual Excel
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBoxAnio.SelectedItem == null)
+                {
+                    MessageBox.Show("Por favor seleccioná un año.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int anio = (int)comboBoxAnio.SelectedItem;
+
+                Creacion_dinamica_carpetas creacion = new Creacion_dinamica_carpetas();
+                string rutaDestino = creacion.Crear_carpeta_reporte();
+                Reporte reporte = new Reporte();
+
+                string rutaExcel = Path.Combine(rutaDestino, $"Reporte_Anual_{anio}.xlsx");
+
+                reporte.generarReporteExcelPorAnio(anio, rutaExcel);
+
+                MessageBox.Show($"Reporte Anual en Excel generado exitosamente en:\n{rutaExcel}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                var proceso = new System.Diagnostics.Process();
+                proceso.StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = rutaExcel,
+                    UseShellExecute = true
+                };
+                proceso.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al generar el reporte Excel anual: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
